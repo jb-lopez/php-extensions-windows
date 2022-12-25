a029e5247ff688cb406578e4723e532f42e93d41
J. B. Lopez <lopezcoder@gmail.com>
2022-12-24 23:23:58 -0800

    Added the tensor dependencies

--- scripts/tensor.ps1
+++ scripts/tensor.ps1
@@ -104,6 +104,8 @@ Function Add-TaskFile() {
     )
     $bat_content = @()
     $bat_content += ""
+    $bat_content += "curl -LO https://github.com/xianyi/OpenBLAS/releases/download/v0.3.21/OpenBLAS-0.3.21-x64.zip"
+    $bat_content += '7z x OpenBLAS-0.3.21-x64.zip -o"..\deps"'
     $bat_content += "call phpize 2>&1"
     $bat_content += "call configure --help"
     $bat_content += "call configure --$config_args --enable-debug-pack 2>&1"
