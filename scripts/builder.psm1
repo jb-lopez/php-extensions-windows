a02c4e0c644d221d0f0604e0ed8412b74141f8d7
Joshua Lopez <lopezcoder@gmail.com>
2023-01-01 17:13:50 -0800

    Moved common elements into a module
    The focus here is to reduce code reuse.

---
+++ scripts/builder.psm1
@@ -0,0 +1,182 @@
+class BuilderParameters
+{
+    [string] $extension
+    [string] $repo
+    [string] $branch
+    [string] $dev_branch
+    [string] $config_args
+    [string] $sdk_version
+    [string] $vs
+    [string] $arch
+    [string] $ts
+    [string] $php
+}
+
+Function PassBuildInformation([BuilderParameters]$buildParams)
+{
+    $script:extension = $buildParams.extension
+    $script:repo = $buildParams.repo
+    $script:branch = $buildParams.branch
+    $script:dev_branch = $buildParams.dev_branch
+    $script:config_args = $buildParams.config_args
+    $script:sdk_version = $buildParams.sdk_version
+    $script:vs = $buildParams.vs
+    $script:arch = $buildParams.arch
+    $script:ts = $buildParams.ts
+    $script:php = $buildParams.php
+}
+
+Function Cleanup([string]$ext_dir, [string]$cache_dir)
+{
+    if (Test-Path $ext_dir)
+    {
+        Remove-Item $ext_dir -Recurse -Force
+    }
+    if (Test-Path $cache_dir)
+    {
+        Remove-Item $cache_dir -Recurse -Force
+    }
+    New-Item -Path $cache_dir -ItemType "directory"
+}
+
+Function Get-Extension([string]$nightly_version, [string]$ext_dir)
+{
+    if ($php -ne $nightly_version)
+    {
+        git clone --branch=$branch $github/$repo.git $ext_dir
+    }
+    else
+    {
+        git clone --branch=$dev_branch $github/$repo.git $ext_dir
+    }
+}
+
+Function Get-BuildPackage([string]$package_zip, [string]$url, [string]$tmp_dir, [string]$package_dir)
+{
+    if (-not(Test-Path $cache_dir\$package_zip))
+    {
+        Invoke-WebRequest $url -OutFile $cache_dir\$package_zip
+    }
+
+    if (-not(Test-Path $package_dir))
+    {
+        Expand-Archive -Path $cache_dir\$package_zip -DestinationPath $cache_dir -Force
+        if ($tmp_dir -ne $package_dir)
+        {
+            Rename-Item -Path $cache_dir\$tmp_dir -NewName $package_dir -Force
+        }
+}
+}
+
+Function Add-TaskFile([string]$filename)
+{
+    $bat_content = @()
+    $bat_content += ""
+    $bat_content += "call phpize 2>&1"
+    $bat_content += "call configure --help"
+    $bat_content += "call configure --$config_args --enable-debug-pack 2>&1"
+    $bat_content += "nmake /nologo 2>&1"
+    $bat_content += "exit %errorlevel%"
+    Set-Content -Encoding "ASCII" -Path $filename -Value $bat_content
+}
+
+Function Get-TSPath()
+{
+    if ($ts -eq 'nts')
+    {
+        return '-nts'
+    }
+
+    return ''
+}
+
+Function Get-ReleaseDirectory()
+{
+    $arch_path = ''
+    if ($arch -eq 'x64')
+    {
+        $arch_path = 'x64'
+    }
+
+    $release_path = 'Release'
+    if ($ts -eq 'ts')
+    {
+        $release_path = 'Release_TS'
+    }
+
+    return [IO.Path]::Combine($arch_path, $release_path)
+}
+
+Function Get-PHPBranch([string]$nightly_version)
+{
+    $php_branch = 'master'
+    if ($php -ne $nightly_version)
+    {
+        $php_branch = "PHP-$php"
+    }
+
+    return $php_branch
+}
+
+Function Build-Extension([string]$trunk, [string]$php_version)
+{
+    $ts_path = Get-TSPath
+    $package_zip = "php-devel-pack-$php_version$ts_path-Win32-$vs-$arch.zip"
+    $tmp_dir = "php-$php_version-devel-$vs-$arch"
+    $package_dir = "php-$php_version$ts_path-devel-$vs-$arch"
+    $url = "$trunk/$package_zip"
+    Get-BuildPackage $package_zip $url $tmp_dir $package_dir
+
+    Set-Location $ext_dir
+    Add-TaskFile "task.bat"
+    $env:PATH = "$cache_dir\$package_dir;$env:PATH"
+    $builder = "$cache_dir\php-sdk-$sdk_version\phpsdk-$vs-$arch.bat"
+    $task = (Get-Item -Path "." -Verbose).FullName + '\task.bat'
+    & $builder -t $task
+}
+
+Function Copy-Extension([string]$workspace)
+{
+    Set-Location $workspace
+    New-Item -Path $extension -ItemType "directory"
+    $release_dir = Get-ReleaseDirectory
+    $ext_path = [IO.Path]::Combine($ext_dir, $release_dir, "php_$extension.dll")
+    Write-Output "Extension Path: $ext_path"
+    if (Test-Path $ext_path)
+    {
+        Copy-Item -Path $ext_path -Destination "$extension\php$php`_$ts`_$arch`_$extension.dll"
+        Get-ChildItem $extension
+    }
+    else
+    {
+        exit 1
+    }
+}
+
+Function RunBuilder([BuilderParameters]$buildParams)
+{
+    PassBuildInformation $buildParams
+
+    $workspace = (Get-Location).Path
+    $cache_dir = "C:\build-cache"
+    $ext_dir = "C:\projects\$extension"
+    $github = "https://github.com"
+    $trunk = "$github/shivammathur/php-builder-windows/releases/download/php$php"
+    $nightly_version = '8.3'
+    $php_branch = Get-PHPBranch $nightly_version
+    $php_version = Invoke-RestMethod "https://raw.githubusercontent.com/php/php-src/$php_branch/main/php_version.h" | Where-Object { $_  -match 'PHP_VERSION "(.*)"' } | Foreach-Object {$Matches[1]}
+    $package_zip = "php-sdk-$sdk_version.zip"
+    $tmp_dir = "php-sdk-binary-tools-$sdk_version"
+    if($sdk_version -eq 'master') {
+        $package_zip = "master.zip"
+        $tmp_dir = "php-sdk-binary-tools-master"
+    }
+    $package_dir = "php-sdk-$sdk_version"
+    $url = "$github/php/php-sdk-binary-tools/archive/$package_zip"
+    
+    Cleanup $ext_dir $cache_dir
+    Get-BuildPackage $package_zip $url $tmp_dir $package_dir
+    Get-Extension $nightly_version $ext_dir
+    Build-Extension $trunk $php_version
+    Copy-Extension $workspace    
+}
--- scripts/tensor.ps1
+++ scripts/tensor.ps1
@@ -18,91 +18,56 @@ param (
     [ValidateNotNull()]
     [ValidateLength(1, [int]::MaxValue)]
     [string]
-    $config_args,
+    $dev_branch,
     [Parameter(Position = 4, Mandatory = $true)]
     [ValidateNotNull()]
     [ValidateLength(1, [int]::MaxValue)]
     [string]
-    $sdk_version,
+    $config_args,
     [Parameter(Position = 5, Mandatory = $true)]
     [ValidateNotNull()]
     [ValidateLength(1, [int]::MaxValue)]
     [string]
-    $vs,
+    $sdk_version,
     [Parameter(Position = 6, Mandatory = $true)]
     [ValidateNotNull()]
     [ValidateLength(1, [int]::MaxValue)]
     [string]
+    $vs,
+    [Parameter(Position = 7, Mandatory = $true)]
+    [ValidateNotNull()]
+    [ValidateLength(1, [int]::MaxValue)]
+    [string]
     $arch,
-    [Parameter(Position = 7, Mandatory = $false)]
+    [Parameter(Position = 8, Mandatory = $true)]
+    [ValidateNotNull()]
+    [ValidateLength(1, [int]::MaxValue)]
     [string]
     $ts,
-    [Parameter(Position = 8, Mandatory = $true)]
+    [Parameter(Position = 9, Mandatory = $true)]
     [ValidateNotNull()]
     [ValidateLength(1, [int]::MaxValue)]
     [string]
     $php
 )
 
-Function Cleanup() {
-    if(Test-Path $ext_dir) {
-        Remove-Item $ext_dir -Recurse -Force
-    }
-    if(Test-Path $cache_dir) {
-        Remove-Item $cache_dir -Recurse -Force
-    }
-    New-Item -Path $cache_dir -ItemType "directory"
+Import-Module ".\scripts\builder.psm1"
+
+$buildParams = @{
+    extension = $extension
+    repo = $repo
+    branch = $branch
+    dev_branch = $dev_branch
+    config_args = $config_args
+    sdk_version = $sdk_version
+    vs = $vs
+    arch = $arch
+    ts = $ts
+    php = $php
 }
 
-Function Get-Extension() {
-    git clone --branch=$branch $github/$repo.git $ext_dir
-    # git clone --branch=$branch $github/$repo.git $ext_dir-project
-    # Move-Item "C:/projects/tensor-project/ext" "C:/projects/tensor"
-}
-
-Function Get-Package {
-    param (
-        [Parameter(Position = 0, Mandatory = $true)]
-        [ValidateNotNull()]
-        [ValidateLength(1, [int]::MaxValue)]
-        [string]
-        $package,
-        [Parameter(Position = 1, Mandatory = $true)]
-        [ValidateNotNull()]
-        [ValidateLength(1, [int]::MaxValue)]
-        [string]
-        $url,
-        [Parameter(Position = 2, Mandatory = $true)]
-        [ValidateNotNull()]
-        [ValidateLength(1, [int]::MaxValue)]
-        [string]
-        $tmp_dir,
-        [Parameter(Position = 3, Mandatory = $true)]
-        [ValidateNotNull()]
-        [ValidateLength(1, [int]::MaxValue)]
-        [string]
-        $package_dir
-    )
-    if (-not (Test-Path $cache_dir\$package)) {
-        Invoke-WebRequest $url -OutFile "$cache_dir\$package"
-    }
-
-    if (-not (Test-Path $package_dir)) {
-        Expand-Archive -Path $cache_dir\$package -DestinationPath $cache_dir -Force
-        if($tmp_dir -ne $package_dir) {
-            Rename-Item -Path $cache_dir\$tmp_dir -NewName $package_dir -Force
-        }
-    }
-}
-
-Function Add-TaskFile1() {
-    param (
-        [Parameter(Position = 0, Mandatory = $true)]
-        [ValidateNotNull()]
-        [ValidateLength(1, [int]::MaxValue)]
-        [string]
-        $filename
-    )
+Function Add-TaskFile1([string]$filename)
+{
     $bat_content = @()
     $bat_content += ""
     $bat_content += "curl -LO https://github.com/xianyi/OpenBLAS/releases/download/v0.3.21/OpenBLAS-0.3.21-x64.zip"
@@ -111,31 +76,18 @@ Function Add-TaskFile1() {
     Set-Content -Encoding "ASCII" -Path $filename -Value $bat_content
 }
 
-Function Add-TaskFile2() {
-    param (
-        [Parameter(Position = 0, Mandatory = $true)]
-        [ValidateNotNull()]
-        [ValidateLength(1, [int]::MaxValue)]
-        [string]
-        $filename
-    )
+Function Add-TaskFile2([string]$filename)
+{
     $bat_content = @()
     $bat_content += ""
     $bat_content += "call phpize 2>&1"
     $bat_content += "call configure --help"
-    # $bat_content += "call configure --$config_args --enable-debug-pack 2>&1"
-    $bat_content += "call configure --$config_args 2>&1"
+    $bat_content += "call configure --$config_args --enable-debug-pack 2>&1"
     Set-Content -Encoding "ASCII" -Path $filename -Value $bat_content
 }
 
-Function Add-TaskFile3() {
-    param (
-        [Parameter(Position = 0, Mandatory = $true)]
-        [ValidateNotNull()]
-        [ValidateLength(1, [int]::MaxValue)]
-        [string]
-        $filename
-    )
+Function Add-TaskFile3([string]$filename)
+{
     $bat_content = @()
     $bat_content += ""
     $bat_content += "nmake /nologo 2>&1"
@@ -143,44 +95,13 @@ Function Add-TaskFile3() {
     Set-Content -Encoding "ASCII" -Path $filename -Value $bat_content
 }
 
-Function Get-TSPath() {
-    if($ts -eq 'nts') {
-        return '-nts'
-    }
-
-    return ''
-}
-
-Function Get-ReleaseDirectory() {
-    $arch_path = ''
-    if($arch -eq 'x64') {
-        $arch_path = 'x64'
-    }
-
-    $release_path = 'Release'
-    if($ts -eq 'ts') {
-        $release_path = 'Release_TS'
-    }
-
-    return [IO.Path]::Combine($arch_path, $release_path)
-}
-
-Function Get-PHPBranch() {
-    $php_branch = 'master'
-    if($php -ne $nightly_version) {
-        $php_branch = "PHP-$php"
-    }
-
-    return $php_branch
-}
-
-Function Build-Extension() {
+Function Build-ExtensionOverride([string]$trunk, [string]$php_version) {
     $ts_path = Get-TSPath
     $package_zip = "php-devel-pack-$php_version$ts_path-Win32-$vs-$arch.zip"
     $tmp_dir = "php-$php_version-devel-$vs-$arch"
     $package_dir = "php-$php_version$ts_path-devel-$vs-$arch"
     $url = "$trunk/$package_zip"
-    Get-Package $package_zip $url $tmp_dir $package_dir
+    Get-BuildPackage $package_zip $url $tmp_dir $package_dir
 
     Set-Location "$ext_dir\ext"
     Add-TaskFile1 "task1.bat"
@@ -203,14 +124,16 @@ Function Build-Extension() {
     & $builder -t $task3
 }
 
-Function Copy-Extension() {
+Function Copy-ExtensionOverride([string]$workspace) {
     Set-Location $workspace
     New-Item -Path $extension -ItemType "directory"
     $release_dir = Get-ReleaseDirectory
     $ext_path = [IO.Path]::Combine($ext_dir, "ext", $release_dir, "php_$extension.dll")
+    $dep_path = [IO.Path]::Combine($ext_dir, "deps", "bin", "libopenblas.dll")
     Write-Output "Extension Path: $ext_path"
     if(Test-Path $ext_path) {
         Copy-Item -Path $ext_path -Destination "$extension\php$php`_$ts`_$arch`_$extension.dll"
+        Copy-Item -Path $dep_path -Destination "$extension\libopenblas.dll"
         Get-ChildItem $extension
         Write-Output "Copied to $extension\php$php`_$ts`_$arch`_$extension.dll"
     } else {
@@ -219,24 +142,7 @@ Function Copy-Extension() {
     }
 }
 
-$workspace = (Get-Location).Path
-$cache_dir = "C:\build-cache"
-$ext_dir = "C:\projects\$extension"
-$github = "https://github.com"
-$trunk = "$github/shivammathur/php-builder-windows/releases/download/php$php"
-$nightly_version = '8.3'
-$php_branch = Get-PHPBranch
-$php_version = Invoke-RestMethod "https://raw.githubusercontent.com/php/php-src/$php_branch/main/php_version.h" | Where-Object { $_  -match 'PHP_VERSION "(.*)"' } | Foreach-Object {$Matches[1]}
-$package_zip = "php-sdk-$sdk_version.zip"
-$tmp_dir = "php-sdk-binary-tools-php-sdk-$sdk_version"
-if($sdk_version -eq 'master') {
-    $package_zip = "master.zip"
-    $tmp_dir = "php-sdk-binary-tools-master"
-}
-$package_dir = "php-sdk-$sdk_version"
-$url = "$github/php/php-sdk-binary-tools/archive/$package_zip"
-Cleanup
-Get-Package $package_zip $url $tmp_dir $package_dir
-Get-Extension
-Build-Extension
-Copy-Extension
+Set-Alias -Name "Build-Extension" -Value "Build-ExtensionOverride"
+Set-Alias -Name "Copy-Extension" -Value "Copy-ExtensionOverride"
+
+RunBuilder $buildParams
